333f5257e1b58aa905c874dd5d8c7a6fb54f7ffb
carlosahs <tachas12@gmail.com>
2022-03-11 17:41:00 -0600

    Add box obstacles

--- xarm_challenge/src/pick_place/src/solution_template.py
+++ xarm_challenge/src/pick_place/src/solution_template.py
@@ -7,7 +7,7 @@ import tf2_ros
 import moveit_commander
 import moveit_msgs.msg
 from moveit_commander.conversions import pose_to_list
-from geometry_msgs.msg import PoseStamped, Pose
+import geometry_msgs.msg
 from path_planner.srv import *
 from tf.transformations import *
 from moveit_msgs.msg import Grasp
@@ -18,7 +18,6 @@ class Planner():
     #TO DO: Initialise move it interface
     # Initialize moveit_commander and a rospy node
     moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('test', anonymous=True)
 
     # Instantiate a RobotCommander object
     robot = moveit_commander.RobotCommander()
@@ -66,7 +65,7 @@ class Planner():
     **************************
     """ 
     # We get the joint values from the group and change some of the values
-    tau = 2.0 * pi
+    tau = 2*pi
     joint_goal = move_group.get_current_joint_values()
     joint_goal[0] = 0
     joint_goal[1] = -tau / 8
@@ -104,13 +103,6 @@ class Planner():
     move_group.clear_pose_targets()
 
 
-    """ 
-    **************************
-    Cartesian Paths
-    **************************
-    """ 
-
-
   def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
     #TO DO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
 
@@ -118,15 +110,46 @@ class Planner():
 
   def addObstacles(self):
     #TO DO: Add obstables in the world
-    
+    # Instantiate a PlanningSceneInterface object
+    scene = moveit_commander.PlanningSceneInterface()
+    """ 
+    **************************
+    Adding Objects to the Planning Scene
+    **************************
+    """ 
     #Cargo names
     targets = ["RedBox",
                "BlueBox",
                "GreenBox"]
+  
+    Rbox_pose = geometry_msgs.msg.PoseStamped()
+    Bbox_pose = geometry_msgs.msg.PoseStamped()
+    Gbox_pose = geometry_msgs.msg.PoseStamped()
+    Rbox_pose.header.frame_id = targets[0]
+    Bbox_pose.header.frame_id = targets[1]
+    Gbox_pose.header.frame_id = targets[2]
+    # RED
+    Rbox_pose.pose.orientation.w = 1.0
+    Rbox_pose.pose.position.z = 0
+    Rbox_name = "RedBox"
+    # BLUE
+    Bbox_pose.pose.orientation.w = 2.0
+    Bbox_pose.pose.position.z = 0
+    Bbox_name = "BlueBox"
+    # GREEN
+    Gbox_pose.pose.orientation.w = 3.0
+    Gbox_pose.pose.position.z = 0
+    Gbox_name = "GreenBox"
+    scene.add_box(Rbox_name, Rbox_pose, size=(0.06, 0.06, 0.06))
+    scene.add_box(Bbox_name, Bbox_pose, size=(0.06, 0.06, 0.06))
+    scene.add_box(Gbox_name, Gbox_pose, size=(0.06, 0.06, 0.06))
+
     #goal names
     boxes = ["DepositBoxGreen",
               "DepositBoxRed",
               "DepositBoxBlue"]
+
+    
 
   def goToPose(self,pose_goal):
     #TO DO: Code used to move to a given position using move it
@@ -150,6 +173,7 @@ class Planner():
 class myNode():
   def __init__(self):
     #TO DO: Initialise ROS and create the service calls
+    rospy.init_node('solution', anonymous=True)
 
     # Good practice trick, wait until the required services are online before continuing with the aplication
     rospy.wait_for_service('RequestGoal')
