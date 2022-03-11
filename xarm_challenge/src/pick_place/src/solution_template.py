ac8d8cf6a8dc194c8119cfa891f2714094fdef6b
carlosahs <tachas12@gmail.com>
2022-03-11 17:25:17 -0600

    Remove non-ASCII characters

--- xarm_challenge/src/pick_place/src/solution_template.py
+++ xarm_challenge/src/pick_place/src/solution_template.py
@@ -44,15 +44,15 @@ class Planner():
     """ 
     # We can get the name of the reference frame for this robot
     planning_frame = move_group.get_planning_frame()
-    print("🦾 Planning frame: %s" % planning_frame)
+    print("Planning frame: %s" % planning_frame)
 
     # We can also print the name of the end-effector link for this group
     eef_link = move_group.get_end_effector_link()
-    print("🦾 End effector link: %s" % eef_link)
+    print("End effector link: %s" % eef_link)
 
     # We can get a list of all the groups in the robot
     group_names = robot.get_group_names()
-    print("🦾 Available Planning Groups:", robot.get_group_names())
+    print("Available Planning Groups:", robot.get_group_names())
 
     # Sometimes for debugging it is useful to print the entire state of the robot
     print("🦾 Printing robot state")
