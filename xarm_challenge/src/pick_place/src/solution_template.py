20cf44aab528d3c54e20e6d92ad95b9b8071d86d
carlosahs <tachas12@gmail.com>
2022-03-12 15:14:02 -0600

    Add MoveIt data members to class

--- xarm_challenge/src/pick_place/src/solution_template.py
+++ xarm_challenge/src/pick_place/src/solution_template.py
@@ -20,88 +20,14 @@ class Planner():
     moveit_commander.roscpp_initialize(sys.argv)
 
     # Instantiate a RobotCommander object
-    robot = moveit_commander.RobotCommander()
+    self.robot = moveit_commander.RobotCommander()
 
     # Instantiate a PlanningSceneInterface object
-    scene = moveit_commander.PlanningSceneInterface()
+    self.scene = moveit_commander.PlanningSceneInterface()
     
     # Instantiate a MoveGroupCommander object
     group_name = "xarm6"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-
-    # Create a DisplayTrajectory ROS publisher
-    display_trajectory_publisher = rospy.Publisher(
-        "/move_group/display_planned_path",
-        moveit_msgs.msg.DisplayTrajectory,
-        queue_size=20,
-    )
-
-    """ 
-    **************************
-    Getting Basic Information
-    **************************
-    """ 
-    # We can get the name of the reference frame for this robot
-    planning_frame = move_group.get_planning_frame()
-    print("Planning frame: %s" % planning_frame)
-
-    # We can also print the name of the end-effector link for this group
-    eef_link = move_group.get_end_effector_link()
-    print("End effector link: %s" % eef_link)
-
-    # We can get a list of all the groups in the robot
-    group_names = robot.get_group_names()
-    print("Available Planning Groups:", robot.get_group_names())
-
-    # Sometimes for debugging it is useful to print the entire state of the robot
-    print("Printing robot state")
-    print(robot.get_current_state())
-    print("")
-
-    
-    """ 
-    **************************
-    Planning to a Joint Goal
-    **************************
-    """ 
-    # We get the joint values from the group and change some of the values
-    tau = 2*pi
-    joint_goal = move_group.get_current_joint_values()
-    joint_goal[0] = 0
-    joint_goal[1] = -tau / 8
-    joint_goal[2] = 0
-    joint_goal[3] = -tau / 4
-    joint_goal[4] = 0
-    joint_goal[5] = tau / 6
-
-    # The go command can be called with joint values, poses, or without any
-    # parameters if you have already set the pose or joint target for the group
-    move_group.go(joint_goal, wait=True)
-
-    # Calling ``stop()`` ensures that there is no residual movement
-    move_group.stop()
-
-
-    """ 
-    **************************
-    Planning to a Pose Goal
-    **************************
-    """ 
-    # Plan a motion for this group to a desired pose for the end-effector
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.4
-    pose_goal.position.y = 0.1
-    pose_goal.position.z = 0.4
-    move_group.set_pose_target(pose_goal)
-
-    # call the planner to compute the plan and execute it
-    plan = move_group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    move_group.stop()
-    # It is always good to clear your targets after planning with poses.
-    move_group.clear_pose_targets()
-
+    self.move_group = moveit_commander.MoveGroupCommander(group_name)
 
   def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
     #TO DO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
@@ -110,8 +36,6 @@ class Planner():
 
   def addObstacles(self):
     #TO DO: Add obstables in the world
-    # Instantiate a PlanningSceneInterface object
-    scene = moveit_commander.PlanningSceneInterface()
     """ 
     **************************
     Adding Objects to the Planning Scene
@@ -125,25 +49,29 @@ class Planner():
     Rbox_pose = geometry_msgs.msg.PoseStamped()
     Bbox_pose = geometry_msgs.msg.PoseStamped()
     Gbox_pose = geometry_msgs.msg.PoseStamped()
+
     Rbox_pose.header.frame_id = "%s" % (targets[0], '1')
     Bbox_pose.header.frame_id = "%s" % (targets[1], '1')
     Gbox_pose.header.frame_id = "%s" % (targets[2], '1')
+
     # RED
     Rbox_pose.pose.orientation.w = 1.0
     Rbox_pose.pose.position.z = 0
-    Rbox_name = "%s" % (targets[0], '1')
+    Rbox_name = targets[0]
+
     # BLUE
     Bbox_pose.pose.orientation.w = 2.0
     Bbox_pose.pose.position.z = 0
-    Bbox_name = "%s" % (targets[1], '1')
+    Bbox_name = targets[1]
+
     # GREEN
     Gbox_pose.pose.orientation.w = 3.0
     Gbox_pose.pose.position.z = 0
-    Gbox_name = "%s" % (targets[2], '1')
+    Gbox_name = targets[2]
 
-    scene.add_box(Rbox_name, Rbox_pose, size=(0.06, 0.06, 0.06))
-    scene.add_box(Bbox_name, Bbox_pose, size=(0.06, 0.06, 0.06))
-    scene.add_box(Gbox_name, Gbox_pose, size=(0.06, 0.06, 0.06))
+    self.scene.add_box(Rbox_name, Rbox_pose, size=(0.06, 0.06, 0.06))
+    self.scene.add_box(Bbox_name, Bbox_pose, size=(0.06, 0.06, 0.06))
+    self.scene.add_box(Gbox_name, Gbox_pose, size=(0.06, 0.06, 0.06))
 
     #goal names
     boxes = ["DepositBoxGreen",
