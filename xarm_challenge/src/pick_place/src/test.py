cf76e0f60867878bc743f868946ea24620f81a04
Oz26 <61431500+Oscar19260@users.noreply.github.com>
2022-03-12 19:17:48 -0600

    Add files via upload

--- xarm_challenge/src/pick_place/src/test.py
+++ xarm_challenge/src/pick_place/src/test.py
@@ -11,52 +11,22 @@ import geometry_msgs.msg
 from path_planner.srv import *
 from tf.transformations import *
 from moveit_msgs.msg import Grasp
-from math import pi
+import math
 
 class Planner():
   def __init__(self):
     #TO DO: Initialise move it interface
     # Initialize moveit_commander and a rospy node
     moveit_commander.roscpp_initialize(sys.argv)
-
     # Instantiate a RobotCommander object
     robot = moveit_commander.RobotCommander()
-
     # Instantiate a PlanningSceneInterface object
     scene = moveit_commander.PlanningSceneInterface()
     
-    # Instantiate a MoveGroupCommander object
-    group_name = "xarm6"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-    self.move_group = move_group
-
-    # Create a DisplayTrajectory ROS publisher
-    display_trajectory_publisher = rospy.Publisher(
-        "/move_group/display_planned_path",
-        moveit_msgs.msg.DisplayTrajectory,
-        queue_size=20,
-    )
-    """ 
-    **************************
-    Getting Basic Information
-    **************************s
-    # We can get the name of the reference frame for this robot
-    planning_frame = move_group.get_planning_frame()
-    print("🦾 Planning frame: %s" % planning_frame)
-
-    # We can also print the name of the end-effector link for this group
-    eef_link = move_group.get_end_effector_link()
-    print("🦾 End effector link: %s" % eef_link)
-
-    # We can get a list of all the groups in the robot
-    group_names = robot.get_group_names()
-    print("🦾 Available Planning Groups:", robot.get_group_names())
-
-    # Sometimes for debugging it is useful to print the entire state of the robot
-    print("🦾 Printing robot state")
-    print(robot.get_current_state())
-    print("")
-    """
+    # Instantiate a MoveGroupCommander objects
+    self.xarm_group = moveit_commander.MoveGroupCommander("xarm6")
+    self.xgripper = moveit_commander.MoveGroupCommander("xarm_gripper")
+    
 
 
   def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
@@ -77,7 +47,6 @@ class Planner():
     targets = ["RedBox",
                "BlueBox",
                "GreenBox"]
-  
     Rbox_pose = geometry_msgs.msg.PoseStamped()
     Bbox_pose = geometry_msgs.msg.PoseStamped()
     Gbox_pose = geometry_msgs.msg.PoseStamped()
@@ -99,33 +68,50 @@ class Planner():
     scene.add_box(Rbox_name, Rbox_pose, size=(0.06, 0.06, 0.06))
     scene.add_box(Bbox_name, Bbox_pose, size=(0.06, 0.06, 0.06))
     scene.add_box(Gbox_name, Gbox_pose, size=(0.06, 0.06, 0.06))
-
     #goal names
     boxes = ["DepositBoxGreen",
               "DepositBoxRed",
               "DepositBoxBlue"]
+  
 
     
-
-  def goToPose(self,pose_goal):
+  def goToPose(self): # pose_goal
     #TO DO: Code used to move to a given position using move it
-    self.move_group.set_pose_target(pose_goal)
-    # call the planner to compute the plan and execute it
-    plan = self.move_group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    self.move_group.stop()
-    # It is always good to clear your targets after planning with poses.
-    self.move_group.clear_pose_targets()
+    pose_target = geometry_msgs.msg.Pose()
+    pose_target.position.x = 0.214284
+    pose_target.position.y = 0.187592
+    pose_target.position.z = 0.001827
+    pose_target.orientation.x = 0.999998
+    pose_target.orientation.y = -0
+    pose_target.orientation.z = -0.00190996
+    pose_target.orientation.w = -0.000413786
+
+    xarm_group.set_pose_target(pose_target)
+    xarm_group.go()
+
+    xgripper.set_named_target("close")
+    xgripper.go(wait = True)
+
+    pose_target = geometry_msgs.msg.Pose()
+    pose_target.position.x = 0.22664
+    pose_target.position.y = -0.411781
+    pose_target.position.z = 0.29226
+    pose_target.orientation.x = 0.999766
+    pose_target.orientation.y = 0.02116288
+    pose_target.orientation.z = -0
+    pose_target.orientation.w = 0
+
+    xgripper.set_named_target("open")
+    xgripper.go(wait = True)
+
 
   def detachBox(self,box_name):
     #TO DO: Open the gripper and call the service that releases the box
-    
     pass
-  
+
 
   def attachBox(self,box_name):
     #TO DO: Close the gripper and call the service that releases the box
-    
     pass
 
 ######################################################################################
@@ -137,11 +123,12 @@ class myNode():
 
     # Good practice trick, wait until the required services are online before continuing with the aplication
     rospy.wait_for_service('RequestGoal')
-    rospy
+    rospy.wait_for_service('AttachObject')
+    
 
   def getGoal(self,action):
     #TO DO: Call the service that will provide you with a suitable target for the movement
-    
+
     pass
 
 
@@ -155,19 +142,7 @@ class myNode():
     #TO DO: Main code that contains the aplication
     self.planner = Planner()
     self.planner.addObstacles()
-
-    # Plan a motion for this group to a desired pose for the end-effector
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.x = 1.0
-    pose_goal.orientation.y = 1.0
-    pose_goal.orientation.z = 1.0
-    pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.8
-    pose_goal.position.y = 0.7
-    pose_goal.position.z = 0.5
-
-    self.planner.goToPose(pose_goal)
-    
+    self.planner.goToPose()
 
     rospy.signal_shutdown("Task Completed")
 
