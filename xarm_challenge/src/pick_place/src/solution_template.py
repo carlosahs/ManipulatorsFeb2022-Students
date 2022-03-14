8f29bd9a8a8d04ec7b0890900373651bae780768
Oz26 <61431500+Oscar19260@users.noreply.github.com>
2022-03-14 14:15:51 -0600

    Add files via upload

--- xarm_challenge/src/pick_place/src/solution_template.py
+++ xarm_challenge/src/pick_place/src/solution_template.py
@@ -16,15 +16,15 @@ from math import pi
 
 #Cargo names
 BOXES = [
+  "GreenBox",
   "RedBox",
   "BlueBox",
-  "GreenBox"
 ]
 #goal names
 DEPOSITS = [
+  "DepositBoxGreen",
   "DepositBoxRed",
-  "DepositBoxBlue",
-  "DepositBoxGreen"
+  "DepositBoxBlue"
 ]
 
 def translation_quaternion_matrix(trans_mat, quat_mat):
@@ -41,70 +41,55 @@ class Planner():
     #TO DO: Initialise move it interface
     # Initialize moveit_commander and a rospy node
     moveit_commander.roscpp_initialize(sys.argv)
-
     # Instantiate a RobotCommander object
     robot = moveit_commander.RobotCommander()
-
     # Instantiate a PlanningSceneInterface object
     scene = moveit_commander.PlanningSceneInterface()
-    
     # Instantiate a MoveGroupCommander object
     xarm_group = moveit_commander.MoveGroupCommander("xarm6")
     xgripper = moveit_commander.MoveGroupCommander("xarm_gripper")
-
     # Create DisplayTrajectory ROS publisher
     display_trajectory_publisher = rospy.Publisher(
         "/move_group/display_planned_path",
         moveit_msgs.msg.DisplayTrajectory,
         queue_size=20
     )
-
+    """
     # Display basic information
     planning_frame = xarm_group.get_planning_frame()
     print("====== Planning frame: %s" % planning_frame)
-
     eef_link = xarm_group.get_end_effector_link()
     print("====== End effector link is : %s" % eef_link)
-
     group_names = robot.get_group_names()
     print("====== Available Planning Groups: %s" % group_names)
-
     print("====== Printing robot state")
     print(robot.get_current_state())
     print('')
-
+    """
     # Instantiate class variables
     self.robot = robot
     self.scene = scene
     self.xarm_group = xarm_group
     self.xgripper = xgripper
     self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
+    # self.planning_frame = planning_frame
+    # self.eef_link = eef_link
+    # self.group_names = group_names
 
   def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
     #TO DO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
     scene = self.scene
-
     start = rospy.get_time()
     seconds = rospy.get_time()
-
     while (seconds - start < timeout) and not rospy.is_shutdown():
         attached_objects = scene.get_attached_objects([box_name])
-
         is_attached = len(attached_objects.keys()) > 0
         is_known = box_name in scene.get_known_object_names()
-
         if (box_is_attached == is_attached) and (box_is_known == is_known):
             return True
-
         rospy.sleep(0.1)
         seconds = rospy.get_time()
-
     return False
-
-
 
   def addObstacles(self):
     #TO DO: Add obstables in the world
@@ -114,7 +99,6 @@ class Planner():
     **************************
     """ 
     scene = self.scene
-
     #Cargo names
     targets = [
         "RedBox",
@@ -127,77 +111,65 @@ class Planner():
         "DepositBoxRed",
         "DepositBoxBlue"
     ]
-
     targets_state = True
-  
     # Red box
     rbox_pose = geometry_msgs.msg.PoseStamped()
     rbox_pose.header.frame_id = targets[0]
-
     rbox_pose.pose.orientation.w = 1.0
     rbox_pose.pose.position.z = 0
     rbox_name = targets[0]
-
     scene.add_box(rbox_name, rbox_pose, size=(0.06, 0.06, 0.06))
-
     targets_state = targets_state and self.wait_for_state_update(targets[0], box_is_known=True)
-
     # Blue box
     bbox_pose = geometry_msgs.msg.PoseStamped()
     bbox_pose.header.frame_id = targets[1]
-
     bbox_pose.pose.orientation.w = 2.0
     bbox_pose.pose.position.z = 0
     bbox_name = targets[1]
-
     scene.add_box(bbox_name, bbox_pose, size=(0.06, 0.06, 0.06))
-
     targets_state = targets_state and self.wait_for_state_update(targets[1], box_is_known=True)
-
     # Green box
     gbox_pose = geometry_msgs.msg.PoseStamped()
     gbox_pose.header.frame_id = targets[2]
-
     gbox_pose.pose.orientation.w = 3.0
     gbox_pose.pose.position.z = 0
     gbox_name = targets[2]
-
     scene.add_box(gbox_name, gbox_pose, size=(0.06, 0.06, 0.06))
-
     targets_state = targets_state and self.wait_for_state_update(targets[2], box_is_known=True)
-
     return targets_state
 
   def goToPose(self,pose_goal):
     #TO DO: Code used to move to a given position using move it
     xarm_group = self.xarm_group
-
     xarm_group.set_pose_target(pose_goal)
     xarm_group.go(wait=True)
-
     xarm_group.stop()
     xarm_group.clear_pose_targets()
 
 
   def detachBox(self,box_name):
     #TO DO: Open the gripper and call the service that releases the box
-    xgripper = self.xgripper
-
-    xgripper.set_named_target("open")
-    xgripper.go(wait = True)
-
-    xgripper.stop()
-    xgripper.clear_pose_targets()
+    try:
+        attach = rospy.ServiceProxy('AttachObject', AttachObject)
+        attach(0, box_name)
+        self.xgripper.set_named_target("open")
+        self.xgripper.go(wait = True)
+        self.xgripper.stop()
+        self.xgripper.clear_pose_targets()
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
 
   def attachBox(self,box_name):
     #TO DO: Close the gripper and call the service that releases the box
-    xgripper = self.xgripper
-
-    xgripper.set_named_target("close")
-    xgripper.go(wait = True)
-
-    xgripper.stop()
-    xgripper.clear_pose_targets()
+    try:
+        attach = rospy.ServiceProxy('AttachObject', AttachObject)
+        attach(1, box_name)
+        self.xgripper.set_named_target("close")
+        self.xgripper.go(wait = True)
+        self.xgripper.stop()
+        self.xgripper.clear_pose_targets()
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
 
 ######################################################################################
 
@@ -205,10 +177,8 @@ class myNode():
   def __init__(self):
     #TO DO: Initialise ROS and create the service calls
     rospy.init_node('solution', anonymous=True)
-
     self.tfBuffer = tf2_ros.Buffer()
     self.listener = tf2_ros.TransformListener(self.tfBuffer)
-
     # Good practice trick, wait until the required services are online before continuing with the aplication
     rospy.wait_for_service('RequestGoal')
     rospy.wait_for_service('AttachObject')
@@ -216,25 +186,21 @@ class myNode():
   def move2goal(self, transform):
       trans = translation_from_matrix(transform)
       quat_rot = quaternion_from_matrix(transform)
-
       goal = geometry_msgs.msg.Pose()
-
+      # Positions
       goal.position.x = trans[0]
       goal.position.y = trans[1]
       goal.position.z = trans[2]
-
+      # Orientations
       goal.orientation.x = 1.0
       goal.orientation.y = 0.0
       goal.orientation.z = 0.0
       goal.orientation.w = 0.0
-
       self.planner.goToPose(goal)
 
   def getGoal(self,action):
     #TO DO: Call the service that will provide you with a suitable target for the movement
-
     pass
-
 
   def tf_goal(self, goal):
     #TO DO:Use tf2 to retrieve the position of the target with respect to the proper reference frame
@@ -247,48 +213,50 @@ class myNode():
         rate.sleep()
         continue
 
-
   def main(self):
     #TO DO: Main code that contains the aplication
     self.planner = Planner()
     self.planner.addObstacles()
+    self.planner.addObstacles()
 
     # Plan a motion for this group to a desired pose for the end-effector
     # Get Box Position
-    gbox = geometry_msgs.msg.Pose()
-
+    box = geometry_msgs.msg.Pose()
     # Boxes and Deposits
     for box, deposit in zip(BOXES, DEPOSITS):
       # Get the box
-      world_tf_pos = self.tf_goal("world")
+      self.planner.attachBox(box)
       xarm_pos_inv = self.tf_goal("link_base")
       box_tf_pos = self.tf_goal(box)
-
+      # Calculus for the arm position
       xarm_trans_inv = get_translation_matrix(xarm_pos_inv.transform.translation)
       xarm_rot_inv = get_quaternion_matrix(xarm_pos_inv.transform.rotation)
       xarm_pos = inverse_matrix(translation_quaternion_matrix(xarm_trans_inv, xarm_rot_inv))
-
+      # Calculus for the box position
       box_trans = get_translation_matrix(box_tf_pos.transform.translation)
       box_rot = get_quaternion_matrix(box_tf_pos.transform.rotation)
-
       box_pos = translation_quaternion_matrix(box_trans, box_rot)
-
+      # Calculus for the movement
       xarm2gbox = np.dot(xarm_pos, box_pos)
       self.move2goal(xarm2gbox)
+      self.planner.attachBox(box)
 
       # Go to deposit
+      xarm_pos_inv = self.tf_goal("link_base")
       dep_tf_pos = self.tf_goal(deposit)
+      # Calculus for the arm position
       xarm_trans_inv = get_translation_matrix(xarm_pos_inv.transform.translation)
       xarm_rot_inv = get_quaternion_matrix(xarm_pos_inv.transform.rotation)
       xarm_pos = inverse_matrix(translation_quaternion_matrix(xarm_trans_inv, xarm_rot_inv))
-
+      # Calculus for the deposit position
       dep_trans = get_translation_matrix(dep_tf_pos.transform.translation)
       dep_rot = get_quaternion_matrix(dep_tf_pos.transform.rotation)
-
       dep_pos = translation_quaternion_matrix(dep_trans, dep_rot)
-
+      dep_pos[2][3] = -0.2
+      # Calculus for the movement
       xarm2gbox = np.dot(xarm_pos, dep_pos)
       self.move2goal(xarm2gbox)
+      self.planner.detachBox(box)
 
     rospy.signal_shutdown("Task Completed")
 
